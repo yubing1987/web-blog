e986b87753b8c2b033f4e05e7ed2b64ada287ef8
yubing1987 <ybjx1987@qq.com>
2018-11-19 23:14:43 +0800

    初始化了spring boot工程

---
+++ src/main/java/com/ybjx/blog/Application.java
@@ -0,0 +1,22 @@
+package com.ybjx.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * spring boot程序启动类
+ * Created by YuBing on 2018/11/19.
+ */
+@SpringBootApplication
+@MapperScan("com.ybjx.blog.dao")
+public class Application {
+
+    /**
+     * spring boot 入口函数
+     * @param args 命令行参数
+     */
+    public static void main(String [] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
