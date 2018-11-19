994746f96457f78483f4c280663ea193239ea878
yubing1987 <ybjx1987@qq.com>
2018-11-19 23:55:30 +0800

    添加了文章接口

--- src/main/java/com/ybjx/blog/common/result/ObjectResult.java
+++ src/main/java/com/ybjx/blog/common/result/ObjectResult.java
@@ -1,7 +1,23 @@
 package com.ybjx.blog.common.result;
 
 /**
+ * 对象形式的返回结果
  * Created by YuBing on 2018/11/19.
  */
 public class ObjectResult<T> extends BaseResult<T> {
+
+    /**
+     * 没有参数的构造函数
+     */
+    public ObjectResult(){
+        super(null);
+    }
+
+    /**
+     * 带内容的构造函数
+     * @param content 返回结果
+     */
+    public ObjectResult(T content) {
+        super(content);
+    }
 }
---
+++ src/main/java/com/ybjx/blog/controller/ArticleController.java
@@ -0,0 +1,23 @@
+package com.ybjx.blog.controller;
+
+import com.ybjx.blog.common.result.PageResult;
+import com.ybjx.blog.dto.ArticleDTO;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 文章相关的接口
+ * Created by YuBing on 2018/11/19.
+ */
+@RequestMapping("/article/")
+@RestController
+public class ArticleController {
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public PageResult<ArticleDTO> getArticlePage(){
+        return null;
+    }
+}
