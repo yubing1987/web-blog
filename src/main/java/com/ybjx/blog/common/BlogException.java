d046dfa3c38d98ec44d48757bd94baedfe77fae4
yubing1987 <ybjx1987@qq.com>
2018-11-20 23:38:37 +0800

    添加了错误编码类和自定义异常类

---
+++ src/main/java/com/ybjx/blog/common/BlogException.java
@@ -0,0 +1,52 @@
+package com.ybjx.blog.common;
+
+/**
+ * 应用自定义异常信息
+ * create by YuBing at 2018/11/20
+ */
+public class BlogException extends Exception {
+    /**
+     * 错误编码
+     */
+    private String code;
+
+    /**
+     * 指定错误编码
+     * @param errorCode 错误编码
+     */
+    public BlogException(ErrorCode errorCode){
+        super(errorCode.getMsg());
+        this.code = errorCode.getCode();
+    }
+
+    /**
+     * 指定错误编码和错误信息
+     * @param errorCode 错误编码
+     * @param msg 错误信息
+     */
+    public BlogException(ErrorCode errorCode, String msg){
+        super(msg);
+        this.code = errorCode.getCode();
+    }
+
+    /**
+     * 指定错误编码和父异常
+     * @param errorCode 错误编码
+     * @param e 父异常
+     */
+    public BlogException(ErrorCode errorCode, Exception e){
+        super(errorCode.getMsg(), e);
+        this.code = errorCode.getCode();
+    }
+
+    /**
+     * 指定错误编码、错误信息和父异常
+     * @param errorCode 错误编码
+     * @param msg 错误信息
+     * @param e 父异常
+     */
+    public BlogException(ErrorCode errorCode, String msg, Exception e){
+        super(msg, e);
+        this.code = errorCode.getCode();
+    }
+}
---
+++ src/main/java/com/ybjx/blog/common/ErrorCode.java
@@ -0,0 +1,41 @@
+package com.ybjx.blog.common;
+
+/**
+ * 错误编码
+ * create by YuBing at 2018/11/20
+ */
+public enum ErrorCode {
+    SYSTEM_ERROR("10000", "系统错误"),
+    OBJECT_EXIST("10001", "数据已经存在")
+    ;
+
+    ErrorCode(String code, String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
+     * 错误编码
+     */
+    private String code;
+    /**
+     * 错误信息
+     */
+    private String msg;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorCode{" +
+                "code='" + code + '\'' +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
